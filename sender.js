2c58ca563970c8a6514b8a0ece600e8104087979
Piki1255 <144703192+Piki1255@users.noreply.github.com>
2024-04-19 20:33:52 +0700

    Add files via upload

---
+++ sender.js
@@ -0,0 +1,37 @@
+//bot token
+
+var telegram_bot_id = "6276277752:AAGSqYvMdG4QotOzGMcVvCPbWumHIWI5epA";
+
+//chat id
+var chat_id = 6100793043;
+var u_name, email, message;
+var ready = function () {
+    u_name = document.getElementById("name").value;
+    email = document.getElementById("email").value;
+    message = document.getElementById("message").value;
+    message = "Name: " + u_name + "\nEmail: " + email + "\nMessage: " + message;
+};
+var sender = function () {
+    ready();
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://api.telegram.org/bot" + telegram_bot_id + "/sendMessage",
+        "method": "POST",
+        "headers": {
+            "Content-Type": "application/json",
+            "cache-control": "no-cache"
+        },
+        "data": JSON.stringify({
+            "chat_id": chat_id,
+            "text": message
+        })
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+    document.getElementById("name").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    return false;
+};
